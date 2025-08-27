b7569507d01ac3608a13a5d17c5ee0a89c7c5be1
pereskokovae <pereskokovae48@gmail.com>
2025-08-27 23:28:59 +0700

    unloaded the database into geojson

--- places/views.py
+++ places/views.py
@@ -1,36 +1,31 @@
 from django.shortcuts import render
+from places.models import Place
 
 
 def index(request):
-    geojson_data = {
-        "type": "FeatureCollection",
-        "features": [
-            {
-                "type": "Feature",
-                "geometry": {
-                    "type": "Point",
-                    "coordinates": [37.62, 55.793676]
-                    },
-                "properties": {
-                    "title": "Легенды Москвы",
-                    "placeId": "moscow_legends",
-                    "detailsUrl": "./places/moscow_legends.json"
+    geojson_data = []
+    places = Place.objects.all()
+    for place in places:
+        latitude = place.lat
+        longitude = place.lng
+        title = place.title
+        geojson_data.append({
+            "type": "FeatureCollection",
+            "features": [
+                {
+                    "type": "Feature",
+                    "geometry": {
+                        "type": "Point",
+                        "coordinates": [longitude, latitude]
+                        },
+                    "properties": {
+                        "title": title,
+                        "placeId": place.id,
+                        "detailsUrl": ""
+                    }
                 }
-            },
-            {
-                "type": "Feature",
-                "geometry": {
-                    "type": "Point",
-                    "coordinates": [37.64, 55.753676]
-                    },
-                "properties": {
-                    "title": "Крыши24.рф",
-                    "placeId": "roofs24",
-                    "detailsUrl": "./places/roofs24.json"
-                }
-            }
-        ]
-    }
+            ]
+            })
     context = {"geojson_data": geojson_data}
 
     return render(request, "index.html", context)
