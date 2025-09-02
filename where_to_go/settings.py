de21b35891c942789307ec7ecc2d609c798b7687
pereskokovae <pereskokovae48@gmail.com>
2025-09-02 18:17:46 +0700

    moved the settings to environment variables

--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -15,9 +15,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = os.environ.get('SERVERNAMES').split(' ')
 
 
 # Application definition
