70477f4d3d835f748b9d750626d9c0ebb39dc361
pereskokovae <pereskokovae48@gmail.com>
2025-09-06 23:30:11 +0700

    updated the admin panel

--- places/admin.py
+++ places/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from .models import Place, Image
 from adminsortable2.admin import SortableStackedInline, SortableAdminBase
 from django.utils.html import format_html
+from django.utils.safestring import mark_safe
 
 
 class StackedInlineImage(SortableStackedInline):
@@ -11,9 +12,18 @@ class StackedInlineImage(SortableStackedInline):
 
     def get_preview(self, obj):
         if obj.images:
-            return format_html(f"<img src='{obj.images.url}', height=200px />")
+            return format_html(
+                "<img src='{}' style='max-height:200px; max-width:300px' />",
+                mark_safe(obj.images.url)
+                )
+
+
+@admin.register(Image)
+class ImageAdmin(admin.ModelAdmin):
+    raw_id_fields = ['place']
 
 
 @admin.register(Place)
 class PlaceAdmin(SortableAdminBase, admin.ModelAdmin):
     inlines = [StackedInlineImage]
+
