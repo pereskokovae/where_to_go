7fa5e14db7ff06582f9748dd23515b3d3e7222ac
pereskokovae <pereskokovae48@gmail.com>
2025-09-07 21:54:01 +0700

    corrected the settings and added a download location

--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -118,7 +118,7 @@ STATICFILES_DIRS = [
     ]
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'static/media')
+MEDIA_ROOT = os.path.join(BASE_DIR, '/static/media')
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
